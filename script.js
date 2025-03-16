efec21727344ed6bb311a49a87cbef43f641ed7f
Vinishireis <nishimuravinicius28@gmail.com>
2025-03-16 18:41:52 -0300

    Atualização de perguntas

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ document.getElementById("quiz-form").addEventListener("submit", function (event)
         q6: "a", // Aula 6
         q7: "b", // Aula 7
         q8: "c", // Aula 8
+        q9: "b", // Aula 1
+        q10: "b", // Aula 1
+        q11: "c", // Aula 2
+        q12: "a", // Aula 2
+        q13: "a", // Aula 3
+        q14: "b", // Aula 3
+        q15: "a", // Aula 4
+        q16: "b", // Aula 4
+        q17: "a", // Aula 5
+        q18: "b", // Aula 5
+        q19: "a", // Aula 6
+        q20: "a", // Aula 6
     };
 
     let score = 0;
